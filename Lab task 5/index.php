f91146f6cdc440448b1a78fb56a8b9611b398bea
Jakaria13 <90682323+Jakaria13@users.noreply.github.com>
2022-04-19 16:52:01 +0600

    Add files via upload

---
+++ Lab task 5/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Product</title>
+</head>
+<body>
+	<h1>Product Based Website</h1>
+	<ul>
+	  <li><a href="/New folder/Lab task 5/views/add-product-view.php">Add Product</a></li>
+	  <li><a href="/New folder/Lab task 5/views/list-product-view.php">Display Product List</a></li>
+	  <li><a href="/New folder/Lab task 5/views/search-product-view.php">Search Product</a></li>
+	</ul>  
+		
+</body>
